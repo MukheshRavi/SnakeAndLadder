2c95a8fcff422867ba42ecee34cd9f2812d7814d
MukheshRavi <mukeshparvathi@gmail.com>
2020-09-29 19:34:04 +0530

    initial position added

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace snake_and_ladders
+{
+    class Program
+    {
+        public static int INITIAL = 0;
+
+       
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("player started at position", INITIAL);
+        }
+    }
+}
