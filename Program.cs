1c077ea11e1265f1ce13dc89015ff5e346b0ff08
MukheshRavi <mukeshparvathi@gmail.com>
2020-09-29 19:55:25 +0530

    option choosen

--- Program.cs
+++ Program.cs
@@ -11,11 +11,35 @@ namespace snake_and_ladders
             int dieNo = r.Next(7);
             return dieNo;
         }
+        public void option()
+        {
+            int position = 0;
+            int dieNo = new Program().rollingDie();
+            Random r = new Random();
+            int option = r.Next(3);
+            switch (option)
+                {
+                case 0:
+                    position = INITIAL + position;
+                    break;
+                case 1:
+                    position = position + dieNo;
+                    break;
+                case 2:
+                    position = position - dieNo;
+                    break;
+                default:
+                    break;
+                       
+
+            }
+
+        }
 
 
         static void Main(string[] args)
         {
-            Console.WriteLine("die value returned is", new Program().rollingDie());
+            
         }
     }
 }
